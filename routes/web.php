b17ff31383656f094e915350722a18434e5e2c25
bharani-developer <bharani.developer@gmail.com>
2021-01-05 12:14:32 +0530

    new

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomePageController@index');
+Route::get('/listing','ListingPageController@index');
+Route::get('/details','DetailsPageController@index');
+
